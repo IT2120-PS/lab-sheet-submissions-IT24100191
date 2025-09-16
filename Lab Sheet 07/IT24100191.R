98b401808626e2c8d94920ff6ea5cc343e7406e8
IT24100191 <it24100191@my.sliit.lk>
2025-09-16 20:08:32 +0530

    add IT24100191_Lab_7

---
+++ Lab Sheet 07/IT24100191.R
@@ -0,0 +1,11 @@
+## 01)
+punif(25, min = 0, max = 40) - punif(10, min = 0, max = 40)
+
+## 02)
+pexp(2, (1 / 3))
+
+## 03)
+# i)
+pnorm(130, 100, 15, lower.tail = FALSE)
+# ii)
+qnorm(0.95, 100, 15)
